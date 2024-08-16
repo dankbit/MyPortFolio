d4b28fd38cac4fe542498985d1b2858982a8e516
vanshmistry <ufvu@gmail.com>
2024-08-16 22:05:22 +0530

    Removed the sidebar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -73,101 +73,10 @@ const Header = () => {
           </div>
           {/* Topbar */}
         </div>
-        <div className='menuWrap hidden lg:flex flex-col items-center justify-between fixed left-[3.4375rem] top-[5.625rem] bottom-[5.625rem] border-2 border-solid border-accent rounded-full z-[9999] w-[6.25rem] px-5 py-[3.125rem]'>
-          {!sideMenu && (
-            <span className='mirror' onClick={toggleMenu}>
-              <CgMenuRight
-                className='text-white cursor-pointer hover:text-accent'
-                size={32}
-              ></CgMenuRight>
-            </span>
-          )}
-          <div className='flex flex-col items-center justify-center gap-8 menuInner'>
-            <SocialLinks></SocialLinks>
-            <span className='h-[2.5rem] sm:h-[3rem] md:h-[5.625rem] bg-accent w-[1px]'></span>
-            <h5
-              className='uppercase text-white font-Poppins font-bold text-[1rem]'
-              style={{
-                textOrientation: 'mixed',
-                writingMode: 'vertical-rl',
-                transform: 'matrix(-1, 0, 0, -1, 0, 0)',
-              }}
-            >
-              Follow Me
-            </h5>
-          </div>
+        
           {/* Menu Wrap */}
-        </div>
-        <div
-          className='sideMenu bg-[#000] fixed transition-all ease-in-out duration-300 z-[9999] top-0 h-screen w-screen flex justify-center items-center px-8 py-16 overflow-y-scroll'
-          style={{
-            opacity: sideMenu ? '1' : '0',
-            visibility: sideMenu ? 'visible' : 'hidden',
-          }}
-        >
-          <span className='mirror absolute right-10 top-4' onClick={toggleMenu}>
-            <CgClose
-              className='text-white cursor-pointer hover:text-accent'
-              size={40}
-            ></CgClose>
-          </span>
-          <ul className='flex flex-col text-4xl gap-10 w-120'>
-            <li>
-              <Link
-                to='/'
-                className='text-white hover:text-accent'
-                onClick={toggleMenu}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                to='/about'
-                className='text-white hover:text-accent'
-                onClick={toggleMenu}
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                to='/service'
-                className='text-white hover:text-accent'
-                onClick={toggleMenu}
-              >
-                Service
-              </Link>
-            </li>
-            <li>
-              <Link
-                to='/experience'
-                className='text-white hover:text-accent'
-                onClick={toggleMenu}
-              >
-                Experience
-              </Link>
-            </li>
-            <li>
-              <Link
-                to='/project'
-                className='text-white hover:text-accent'
-                onClick={toggleMenu}
-              >
-                Project
-              </Link>
-            </li>
-            <li>
-              <Link
-                to='/contact'
-                className='text-white hover:text-accent'
-                onClick={toggleMenu}
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
+        
+        
         {/* Header */}
       </header>
       <ResponsiveHeader></ResponsiveHeader>
